13f65e89e018b5b0865ed4374d2cc54f1b3ac333
Mark Howe <mvahowe@gmail.com>
2020-10-27 17:13:16 +0100

    uW selectors and selectorString

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ const { ProsKomma } = require('proskomma');
 
 class UWProsKomma extends ProsKomma {
 
+    constructor() {
+        super();
+        this.selectors = [
+            {
+                name: "org",
+                type: "string",
+                regex: "^[^\\s]+$"
+            },
+            {
+                name: "lang",
+                type: "string",
+                regex: "^[^\\s]+$"
+            },
+            {
+                name: "abbr",
+                type: "string",
+                regex: "^[a-z0-9]+$"
+            }
+        ];
+        this.validateSelectors();
+        this.filters = {};
+        this.customTags = {
+            heading: [],
+            paragraph: [],
+            char: [],
+            word: [],
+            intro: [],
+            introHeading: []
+        }
+        this.emptyBlocks = [];
+    }
+
+    selectorString(docSetSelectors) {
+        return `${docSetSelectors.org}/${docSetSelectors.lang}_${docSetSelectors.abbr}`;
+    }
 }
 
 module.exports = {UWProsKomma}
--- test/code/smoke.js
+++ test/code/smoke.js
@@ -1,4 +1,7 @@
 const test = require('tape');
+const fse = require('fs-extra');
+const path = require('path');
+
 const {UWProsKomma} = require('../../index')
 
 const testGroup = "Smoke";
@@ -14,3 +17,43 @@ test(
         }
     }
 );
+
+test(
+    `Root Selector Query (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(1);
+            const pk = new UWProsKomma();
+            const query = '{selectors { name type regex } }';
+            const result = await pk.gqlQuery(query);
+            t.equal(result.errors, undefined);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+);
+
+test(
+    `DocSet Selector Query (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(3);
+            const pk = new UWProsKomma();
+            const content = fse.readFileSync(path.resolve(__dirname, "../test_data/usfm/ust_psa_1.usfm"));
+            pk.importDocument(
+                {"org": "unfoldingWord", "lang": "eng", "abbr": "ust"},
+                "usfm",
+                content
+            );
+            const query = '{docSets { selectors { key value } selectorString } }';
+            const result = await pk.gqlQuery(query);
+            t.equal(result.errors, undefined);
+            const docSet = result.data.docSets[0];
+            t.equal(docSet.selectors.length, 3);
+            t.equal(docSet.selectorString, "unfoldingWord/eng_ust");
+        } catch (err) {
+            console.log(err)
+        }
+    }
+);
+
