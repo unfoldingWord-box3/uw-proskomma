ef16a48d1855e7e316a307ed2391253d76a356df
Mark Howe <mvahowe@gmail.com>
2020-10-28 15:27:36 +0100

    Test for s5 substitution

--- test/code/smoke.js
+++ test/code/smoke.js
@@ -6,6 +6,19 @@ const {UWProsKomma} = require('../../index')
 
 const testGroup = "Smoke";
 
+const pkWithDoc = () => {
+    const pk = new UWProsKomma();
+    const content = fse.readFileSync(path.resolve(__dirname, "../test_data/usfm/ust_psa_1.usfm")).toString();
+    pk.importDocument(
+        {"org": "unfoldingWord", "lang": "eng", "abbr": "ust"},
+        "usfm",
+        content
+    );
+    return pk;
+}
+
+const itemFragment = '{ ... on Token { subType chars } ... on Scope { itemType label } ... on Graft { subType sequenceId } }';
+
 test(
     `Instantiate (${testGroup})`,
     async function (t) {
@@ -38,19 +51,32 @@ test(
     async function (t) {
         try {
             t.plan(3);
-            const pk = new UWProsKomma();
-            const content = fse.readFileSync(path.resolve(__dirname, "../test_data/usfm/ust_psa_1.usfm")).toString();
-            pk.importDocument(
-                {"org": "unfoldingWord", "lang": "eng", "abbr": "ust"},
-                "usfm",
-                content
-            );
+            const pk = pkWithDoc();
             const query = '{docSets { selectors { key value } selectorString } }';
             const result = await pk.gqlQuery(query);
             t.equal(result.errors, undefined);
             const docSet = result.data.docSets[0];
             t.equal(docSet.selectors.length, 3);
             t.equal(docSet.selectorString, "unfoldingWord/eng_ust");
+        } catch (err) {
+            console.log(err)
+        }
+    }
+);
+
+test(
+    `s5 heading to ts milestone (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(4);
+            const pk = pkWithDoc();
+            const query = `{documents { mainSequence { blocks { is { label } bg { subType } } } } }`;
+            const result = await pk.gqlQuery(query);
+            t.equal(result.errors, undefined);
+            const blocks = result.data.documents[0].mainSequence.blocks;
+            t.equal(blocks.map(b => b.is).map(is => is.map(s => s.label)).filter(l => l.includes("milestone/ts")).length, 3);
+            t.equal(blocks.map(b => b.bg).map(bg => bg.map(g => g.subType)).filter(s => s.includes("title")).length, 1);
+            t.equal(blocks.map(b => b.bg).map(bg => bg.map(g => g.subType)).filter(s => s.includes("heading")).length, 0);
         } catch (err) {
             console.log(err)
         }
